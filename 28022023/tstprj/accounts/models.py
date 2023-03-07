f80b90bdf80f23404fa04afdbd485cd6428f834a
Peter Kurishev <peter@kurishev.ru>
2023-03-07 23:08:19 +0300

    Кода урока от 7 марта 2023

--- 28022023/tstprj/accounts/models.py
+++ 28022023/tstprj/accounts/models.py
@@ -1,3 +1,18 @@
+"""Модуль моделей приложения аккаунтов"""
+from datetime import datetime
+
 from django.db import models
 
-# Create your models here.
+
+class Accounts(models.Model):
+    """Модель аккаунтов (мест где храняться денежные средства)."""
+
+    name = models.CharField(max_length=128)
+    current_sum = models.IntegerField()  # сумма счета в копейках
+    starting_sum = models.IntegerField()
+    creation_date = models.DateTimeField()
+    modification_date = models.DateTimeField(default=datetime.now())
+
+    def __str__(self):
+        """Имя: Текузая сумма."""
+        return f"{self.name}: {self.current_sum}"
