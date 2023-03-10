7ceb9f4453c8fe6e309ce84c98c89a16d7712e0c
Peter Kurishev <peter@kurishev.ru>
2023-03-10 06:33:27 +0300

    Файлы с занятия 9 марта 2023 года

--- 28022023/tstprj/accounts/admin.py
+++ 28022023/tstprj/accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Accounts
+from .models import Accounts, MoneyMovement
 
 admin.site.register(Accounts)
+admin.site.register(MoneyMovement)
--- 28022023/tstprj/accounts/models.py
+++ 28022023/tstprj/accounts/models.py
@@ -1,4 +1,5 @@
 """Модуль моделей приложения аккаунтов"""
+from django.db.models.deletion import CASCADE
 from datetime import datetime
 
 from django.db import models
@@ -14,5 +15,31 @@ class Accounts(models.Model):
     modification_date = models.DateTimeField(default=datetime.now())
 
     def __str__(self):
-        """Имя: Текузая сумма."""
+        """Имя: Текущая сумма."""
         return f"{self.name}: {self.current_sum}"
+
+
+class MoneyMovement(models.Model):
+    """Модель для фиксации движения денежных средств"""
+    source_account = models.ForeignKey("Accounts", on_delete=CASCADE, related_name='srcacc', null=True, blank=True)
+    destination_account = models.ForeignKey("Accounts", on_delete=CASCADE, related_name='dstacc')
+    sum = models.IntegerField(verbose_name="Сумма в копейках")
+    subject = models.CharField(verbose_name="Причина движения", max_length=128)
+
+    def __str__(self):
+        return f"{self.source_account} -> {self.destination_account} ({self.sum})"
+
+    def save(self):
+        if self.source_account is not None:
+            src = self.source_account.current_sum
+            self.source_account.current_sum = src - self.sum
+
+        dst = self.destination_account.current_sum
+        self.destination_account.current_sum = dst + self.sum
+
+        if self.source_account is not None:
+            self.source_account.save()
+
+        self.destination_account.save()
+
+        super(MoneyMovement, self).save()
