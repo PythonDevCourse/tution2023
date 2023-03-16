8feb5f3b953d4374bbd6f392785e0d459c5c345b
Peter Kurishev <peter@kurishev.ru>
2023-03-16 20:56:15 +0300

    Занятие 16 марта 2023

--- 14032023/test.py
+++ 14032023/test.py
@@ -2,7 +2,7 @@
 Docstring 2
 
 >>> compact ([1,2,3])
-'1,2,3'
+'1-3'
 """
 
 
@@ -15,29 +15,61 @@ def compact(L):
 
     >>> compact([1,3,5])
     '1,3,5'
+    >>> compact([1,2,3,5])
+    '1-3,5'
+    >>> compact([1,3,5,6,10,11,12,16,17,18])
+    '1-6,10-12,16-18'
     """
     if L == []:
         return ""
     elif L is None:
         return ""
+
+    in_interval = False
+
+    interval_start = None
+
     L = sorted(L)
-    prev = L[0]
-    out_str = str(prev)
-    for i in L[1:]:
-        if i - prev == 1:
+    prev = None
+    out_str = ""
+
+    last_elem = L[-1]
+
+    for i in L:
+        if prev is None:
+            out_str = f"{i}"
             prev = i
             continue
-        if i - prev != 1:
-            # out_str = out_str + "-" + str(prev) + "," + str(i)
-            out_str = out_str + "," + str(i)
+        if i - prev == 1 and not in_interval:
+            interval_start = prev
+            in_interval = True
+            print(f"Попали в интервал {i} {prev}")
             prev = i
+            continue
+        if i - prev != 1 and in_interval:
+            if in_interval and prev - interval_start != 1:
+                out_str = out_str + f"-{prev},{i}"
+            if in_interval and prev - interval_start == 1:
+                out_str = out_str + f",{prev},{i}"
+            in_interval = False
+            interval_start = None
+            continue
+        if i - prev != 1 and not in_interval:
+            out_str += f",{i}"
+            continue
+        if i == last_elem and in_interval:
+            out_str += f"-{i}"
+        if i == last_elem and not in_interval:
+            out_str += f",{i}"
+        prev = i
     return out_str
 
 
 if __name__ == "__main__":
-    import doctest
+    # import doctest
 
-    doctest.testmod()
+    # doctest.testmod()
 
-    a = compact([1, 3, 5])
+    a = compact([1, 2, 3, 5])
+    print("==================")
     print(a)
